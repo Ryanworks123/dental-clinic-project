e17322ae511f6b9ef0dd2f68c7a2668102809457
Ryan Roferos <ryanroferos.work@gmail.com>
2026-08-16 19:43:12 +0800

    Serve nested appointment actions in production

---
+++ api/appointments/[id]/reschedule.js
@@ -0,0 +1,4 @@
+import app from '../../../server/index.js';
+
+// Production handler for PATCH /api/appointments/:id/reschedule.
+export default app;
---
+++ api/notifications/[id]/read.js
@@ -0,0 +1,4 @@
+import app from '../../../server/index.js';
+
+// Production handler for PATCH /api/notifications/:id/read.
+export default app;
