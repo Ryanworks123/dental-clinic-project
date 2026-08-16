c4b3c17b2274e1f9c7f1aef7dcc2824c7217ac8c
Ryan Roferos <ryanroferos.work@gmail.com>
2026-08-16 19:43:36 +0800

    Resolve Vercel API route conflicts

--- api/appointments/[...path].js
+++
@@ -1,4 +0,0 @@
-import app from '../../server/index.js';
-
-// Handles nested appointment actions such as rescheduling and status updates.
-export default app;
--- api/notifications/[...path].js
+++
@@ -1,4 +0,0 @@
-import app from '../../server/index.js';
-
-// Handles notification actions such as marking a notification as read.
-export default app;
